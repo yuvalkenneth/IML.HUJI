7feecf46f6abbfe43e441d9eb1f214f965f01eb9
yuvalkenneth <62839011+yuvalkenneth@users.noreply.github.com>
2022-04-19 18:56:00 +0300

    ex-3

--- exercises/classifiers_evaluation.py
+++ exercises/classifiers_evaluation.py
@@ -1,4 +1,5 @@
 import sys
+from math import atan2
 from typing import Tuple
 
 import numpy as np
@@ -45,6 +46,30 @@ def load_dataset(filename: str) -> Tuple[np.ndarray, np.ndarray]:
     return data[:, 0:-1], data[:, -1]
 
 
+def get_ellipse(mu: np.ndarray, cov: np.ndarray):
+    """
+    Draw an ellipse centered at given location and according to specified covariance matrix
+    Parameters
+    ----------
+    mu : ndarray of shape (2,)
+        Center of ellipse
+    cov: ndarray of shape (2,2)
+        Covariance of Gaussian
+    Returns
+    -------
+        scatter: A plotly trace object of the ellipse
+    """
+    l1, l2 = tuple(np.linalg.eigvalsh(cov)[::-1])
+    theta = atan2(l1 - cov[0, 0], cov[0, 1]) if cov[0, 1] != 0 else (
+        np.pi / 2 if cov[0, 0] < cov[1, 1] else 0)
+    t = np.linspace(0, 2 * np.pi, 100)
+    xs = (l1 * np.cos(theta) * np.cos(t)) - (l2 * np.sin(theta) * np.sin(t))
+    ys = (l1 * np.sin(theta) * np.cos(t)) + (l2 * np.cos(theta) * np.sin(t))
+
+    return go.Scatter(x=mu[0] + xs, y=mu[1] + ys, mode="lines",
+                      marker_color="black")
+
+
 def run_perceptron():
     """
     Fit and plot fit progression of the Perceptron algorithm over both the linearly separable and inseparable datasets
@@ -73,9 +98,7 @@ def compare_gaussian_classifiers():
     """
     for f in ["gaussian1.npy", "gaussian2.npy"]:
         # Load dataset
-        data = np.load(f)
-        X = data[:, 0:-1]
-        y = data[:, -1]
+        X, y = load_dataset(f)
         # Fit models and predict over training set
         gaussian = GaussianNaiveBayes()
         gaussian.fit(X, y)
@@ -89,10 +112,21 @@ def compare_gaussian_classifiers():
         # Plot a figure with two suplots, showing the Gaussian Naive Bayes predictions on the left and LDA predictions
         # on the right. Plot title should specify dataset used and subplot titles should specify algorithm and accuracy
         plot_models(X, y, gaussian_pred, lda_pred, gaussian_accuracy,
-                    lda_accuracy)
+                    lda_accuracy, gaussian.mu_, gaussian.vars_,
+                    linear_discriminant.mu_, linear_discriminant.cov_)
 
 
-def plot_models(X, y, gauss_pred, lda_pred, gauss_acc, lda_acc):
+def create_shapes(fig, gauss_exp, gauss_var, lda_exp, lda_var):
+    for i in range(3):
+        fig.add_trace(get_ellipse(gauss_exp[i], np.diag(gauss_var[i])), row=1,
+                      col=1)
+        fig.add_trace(get_ellipse(lda_exp[i], lda_var), row=1, col=2)
+        fig.add_trace(add_x_mark(gauss_exp[i]), row=1, col=1)
+        fig.add_trace(add_x_mark(lda_exp[i]), row=1, col=2)
+
+
+def plot_models(X, y, gauss_pred, lda_pred, gauss_acc, lda_acc, gauss_exp,
+                gauss_var, lda_exp, lda_var):
     fig = make_subplots(rows=1, cols=2,
                         subplot_titles=(f"gaussian naive bayes\naccuracy:"
                                         f"{gauss_acc}",
@@ -104,9 +138,26 @@ def plot_models(X, y, gauss_pred, lda_pred, gauss_acc, lda_acc):
     fig.add_trace(
         go.Scatter(x=X[:, 0], y=X[:, 1], mode='markers', marker=dict(
             color=lda_pred, symbol=y, size=7)), row=1, col=2)
+    create_shapes(fig, gauss_exp, gauss_var, lda_exp, lda_var)
     fig.update_layout(height=800, width=1500,
                       title_text="Naive gaussian bayes VS LDA")
     fig.show()
+
+
+def add_x_mark(mu):
+    """
+    Draw an ellipse centered at given location and according to specified covariance matrix
+    Parameters
+    ----------
+    mu : ndarray of shape (2,)
+        Center of samples
+    Returns
+    -------
+        scatter: A plotly trace object of the X mark
+    """
+    return go.Scatter(x=[mu[0]], y=[mu[1]], mode='markers',
+                      marker=dict(
+                          color='black', symbol='cross', size=8))
 
 
 if __name__ == '__main__':
